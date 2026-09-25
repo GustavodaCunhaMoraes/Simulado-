50b7b01bc32911d0267d747a8922670fb233172e
Gustavo <@gustavodacunhamoraesgcm.gmail.com>
2026-09-24 23:35:29 -0300

    Atualizando arquivos

--- QUESTAO 2/Retangulo.java
+++ QUESTAO 2/Retangulo.java
@@ -2,59 +2,45 @@ public class Retangulo {
     private double largura;
     private double altura;
 
-    public Retangulo(double largura, double altura) {
-        setLargura(largura);
-        setAltura(altura);
+    public Retangulo(double larg, double alt) {
+        this.largura = larg;
+        this.altura = alt;
+    }
+
+    public Retangulo() {
+        this(1.0, 1.0);
     }
 
     public double getLargura() {
         return largura;
     }
 
-    public void setLargura(double largura) {
-        if (largura > 0) {
-            this.largura = largura;
-        } else {
-            this.largura = 1.0;
-        }
-    }
-
     public double getAltura() {
         return altura;
     }
 
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
     public void setAltura(double altura) {
-        if (altura > 0) {
-            this.altura = altura;
-        } else {
-            this.altura = 1.0;
-        }
+        this.altura = altura;
     }
 
     public double calcularArea() {
-        return largura * altura;
-    }
-
-    public double calcularPerimetro() {
-        return 2 * (largura + altura);
+        return altura * largura;
     }
 
     public boolean isQuadrado() {
-        return largura == altura;
+        return altura == largura;
     }
-}
 
-class Main {
     public static void main(String[] args) {
-        Retangulo r1 = new Retangulo(5.0, 5.0);
-
-        System.out.println("Área: " + r1.calcularArea());
-        System.out.println("Perímetro: " + r1.calcularPerimetro());
-
-        if (r1.isQuadrado()) {
-            System.out.println("É um quadrado!");
+        Retangulo ret = new Retangulo(5.0, 5.0);
+        if (ret.isQuadrado() == true) {
+            System.out.println("O retangulo e quadrado e tem area: " + ret.calcularArea());
         } else {
-            System.out.println("Não é um quadrado.");
+            System.out.println("Nao e quadrado");
         }
     }
 }
